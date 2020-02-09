f8044acc4f5ab979d46b9ebe302337659e81a156
Charlie Zhang <sunnypig2002@gmail.com>
2020-02-09 16:17:26 +0800

    add loading to login page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-import { ToastController } from '@ionic/angular';
+import {
+  ToastController,
+  LoadingController,
+} from '@ionic/angular';
 import { LoginService } from '../../services/login.service';
 import { StorageService } from '../../services/storage.service';
 
@@ -16,6 +19,7 @@ export class LoginPage implements OnInit {
 
   constructor(
     protected location: Location,
+    protected loadingController: LoadingController,
     protected toastController: ToastController,
     protected loginService: LoginService,
     protected storageService: StorageService,
@@ -44,12 +48,19 @@ export class LoginPage implements OnInit {
       toast.present();
       return;
     }
-
+    const loading = await this.loadingController.create({
+      spinner: null,
+      // duration: 5000,
+      message: 'Logging, Please wait...',
+      translucent: true,
+    });
+    await loading.present();
     try {
       await this.loginService.login(
         this.username,
         this.password,
       );
+
       this.location.back();
     } catch (e) {
       const toast = await this.toastController.create({
@@ -58,6 +69,8 @@ export class LoginPage implements OnInit {
       });
       toast.present();
       return;
+    } finally {
+      loading.dismiss();
     }
   }
 
